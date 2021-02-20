9a7540536e92ec70782a461cb17370b11c582ea1
Alfio Puglisi <alfio.puglisi@inaf.it>
2021-02-20 12:24:13 +0100

    Fixed configuration defaults for nomad1

--- catalog_conf.py
+++ catalog_conf.py
@@ -2,6 +2,6 @@
 gsc23_exe = '/DATA_BACKUP/catalogs/GSC23/orig/bin/gsc23'
 gsc23_dir = '/DATA_BACKUP/catalogs/GSC23/orig'
 
-nomad_exe = '/DATA_BACKUP/catalogs/NOMAD1/orig/bin/nomad1'
-nomad_dir = '/DATA_BACKUP/catalogs/NOMAD1/orig/bindat'
+nomad1_exe = '/DATA_BACKUP/catalogs/NOMAD1/orig/bin/nomad1'
+nomad1_dir = '/DATA_BACKUP/catalogs/NOMAD1/orig/bindat'
 
