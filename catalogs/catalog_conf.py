7a4e924d8dc1139251ace7fd2987bcecfb8b6c34
Alfio Puglisi <alfio.puglisi@inaf.it>
2021-05-28 12:31:53 +0200

    Added import of local_conf.py

--- catalogs/catalog_conf.py
+++ catalogs/catalog_conf.py
@@ -5,3 +5,9 @@ gsc23_dir = '/DATA_BACKUP/catalogs/GSC23/orig'
 nomad1_exe = '/DATA_BACKUP/catalogs/NOMAD1/orig/bin/nomad1'
 nomad1_dir = '/DATA_BACKUP/catalogs/NOMAD1/orig/bindat'
 
+# Custom config
+try:
+   from catalogs.local_conf import *
+except ImportError:
+   pass
+
